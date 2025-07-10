0e51aa874fcc9aa31647f7e149b0dafdedb57478
ADMIN <dishajatkar@gmail.com>
2025-07-10 10:51:12 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -3,17 +3,37 @@ const express = require("express");
 const app = express();
 
 // request handler: handle incoming request
-app.use("/", (req, res) => {
-  res.send("Welcome to the Express server!");
+app.use("/user", (req, res) => {
+  res.send("Hello, User!");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello, World!");
+// this will handle GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Disha", lastName: "Jatkar" });
 });
+
+app.post("/user", (req, res) => {
+  console.log("User data received");
+  res.send({ message: "User created successfully!" });
+});
+
+app.delete("/user", (req, res) => {
+  console.log("User deleted");
+  res.send({ message: "User deleted successfully!" });
+});
+
+// this will match all the http method API calls to /test
+// app.use("/test", (req, res) => {
+//   res.send("Hello, World!");
+// });
 
 app.use("/hello", (req, res) => {
   res.send("Hello, Express!");
 });
+
+// app.use("/", (req, res) => {
+//   res.send("Welcome to the Express server!");
+// });
 
 // Craeted server and listening on post 3000
 app.listen(3000, () => {
