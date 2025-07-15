06a4a8803bb97fc9445fb6b1a689186905007f9a
ADMIN <dishajatkar@gmail.com>
2025-07-15 15:28:00 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -3,17 +3,12 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Yuvraj",
-    lastName: "Singh",
-    emailId: "yuvrajt@gmail.com",
-    password: "Yuvraj123",
-  };
+app.use(express.json()); // Middleware to parse JSON bodies
 
+app.post("/signup", async (req, res) => {
   try {
     // Create a new instance of the User model
-    const user = new User(userObj);
+    const user = new User(req.body);
     // Save the user to the database. This returns a Promise
     await user.save();
     res.send("User added successfully");
@@ -21,6 +16,74 @@ app.post("/signup", async (req, res) => {
     res.status(400).send("Error adding user: " + err.message);
   }
 });
+
+// Get user by emailId
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.findOne({ emailId: req.body.emailId });
+    if (!users) {
+      return res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Error fetching user: " + err.message);
+  }
+});
+
+// Feed API - GET /feed - get all users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      return res.status(404).send("No users found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Error fetching feed: " + err.message);
+  }
+});
+
+// Delete user by userId
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Error deleting user: " + err.message);
+  }
+});
+
+// Update data of user
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const userId = data.userId;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data,{
+      runValidators: true, // Ensure that validation rules are applied during update
+    });
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("Error updating user: " + err.message);
+  }
+});
+
+// Update user with emailId
+// app.patch("/user", async (req, res) => {
+//   const data = req.body;
+//   const emailId = data.emailId;
+//   console.log("EmailId to update:", emailId);
+//   delete data.emailId; // Remove emailId from data to avoid updating it
+//   console.log("Data to update:", data);
+//   try {
+//     const user = await User.findOneAndUpdate({ emailId: emailId }, data);
+//     res.send("User updated successfully");
+//   } catch {
+//     res.status(400).send("Error updating user: " + err.message);
+//   }
+// });
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true, // Ensure emailId is unique
+      lowercase: true, // Store emailId in lowercase
+      trim: true, // Remove whitespace from emailId
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18, // Minimum age requirement
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoURL: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: {
+      type: String,
+      default: "This is default about of user!",
+    },
+    skills: {
+      type: [String], // Array of strings for skills
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 // const User = mongoose.model("User", userSchema);
 module.exports = mongoose.model("User", userSchema);
